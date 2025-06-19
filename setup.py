ead3dbc70ddc86aaf782200e161d57bed8f0cf8f
ekansh <ekansh@duck.com>
2025-06-20 00:04:51 +0530

    latest version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="nterm",
-    version="0.1.5",
+    version="0.1.9",
     author="Neural Nirvana",
     author_email="ekansh@duck.com",
     description="A Terminal based AI reasoning agent with system administration and IoT capabilities",
